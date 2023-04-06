b782e65e319a09a6209478695f53211b0c9458c8
Nikesh Maurya <nikeshinknit@gmail.com>
2023-04-06 16:49:02 +0530

    first commit

---
+++ scripts/finalDeploy.js
@@ -0,0 +1,12 @@
+const hre = require("hardhat");
+
+async function main() {
+    const chai = await hre.ethers.getContractFactory("Coffee");
+    const contract = await chai.deploy();
+    await contract.deployed();
+    console.log("Address of contract: ",contract.address);
+}
+main().catch((error)=>{
+    console.error(error);
+    process.exitCode=1;
+});
